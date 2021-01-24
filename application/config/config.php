421f5e38a0591c0c2cebe7fb99588fd0f08d3dc7
RakotosonFabien <rakotosonfabienmaminirina@gmail.com>
2021-01-24 20:43:13 +0000

    baseUrl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8282/2021/Partie1/ExamenHalfDay_1_2021/';
+//$config['base_url'] = 'http://localhost:8282/2021/Partie1/ExamenHalfDay_1_2021/';
+$config['base_url'] = 'https://rakotosonfabien.github.io/';
 
 /*
 |--------------------------------------------------------------------------
