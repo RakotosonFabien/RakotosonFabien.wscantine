3284d55c506a70a85de8b6bd1e557b533ede0c86
RakotosonFabien <rakotosonfabienmaminirina@gmail.com>
2021-01-26 20:38:08 +0000

    content-update-2

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8282/2021/Partie1/ETU000923/ExamenHalfDay_1_2021/';
-//$config['base_url'] = 'https://rf-webservices-1.herokuapp.com/';
+//$config['base_url'] = 'http://localhost:8282/2021/Partie1/ETU000923/ExamenHalfDay_1_2021/';
+$config['base_url'] = 'https://rf-webservices-1.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
